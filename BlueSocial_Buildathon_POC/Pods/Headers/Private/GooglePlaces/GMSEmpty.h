0b7cc335d63010f63b2d2ec77822c042adef64d3
Chukwunonso Ikeji <williamikeji@gmail.com>
2024-06-30 17:26:00 +0100

    Proprietary technology removed

--- BlueSocial_Buildathon_POC/Pods/Headers/Private/GooglePlaces/GMSEmpty.h
+++
@@ -1,1 +0,0 @@
-../../../GooglePlaces/Sources/GMSEmpty.h
--- BlueSocial_Buildathon_POC/Pods/Target Support Files/Pods-BlueNotificationService/Pods-BlueNotificationService-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BlueNotificationService : NSObject
-@end
-@implementation PodsDummy_Pods_BlueNotificationService
-@end
